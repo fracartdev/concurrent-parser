77552d50474b7c431b390a2ca9edfb1187dbd3ff
Francesco Cartier <francesco.cartier@testudosrl.com>
2021-02-09 10:22:14 +0100

    reinit

--- main.go
+++ main.go
@@ -1,39 +1,91 @@
 package main
 
 import (
+	"bytes"
+	"encoding/csv"
+	"encoding/json"
+	"flag"
 	"fmt"
-	"time"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
 )
 
-func worker(id int, jobs <-chan int, results chan<- int) {
-	// parsing del CSV to JSON, per ora simulo con un timeout
-	for j := range jobs {
-		fmt.Println("worker", id, "started job", j)
-		time.Sleep(time.Second)
-		fmt.Println("worker", id, "finished job", j)
-		results <- j * 2
-	}
+func main() {
+	path := flag.String("path", "./data.csv", "Percorso del file")
+	flag.Parse()
+	fileBytes, fileNPath := ReadCSV(path)
+	SaveFile(fileBytes, fileNPath)
+	fmt.Println(strings.Repeat("=", 10), "Done", strings.Repeat("=", 10))
 }
 
-func main() {
-	const numJobs = 5
-	jobs := make(chan int, numJobs)
-	results := make(chan int, numJobs)
+// ReadCSV to read the content of CSV File
+func ReadCSV(path *string) ([]byte, string) {
+	csvFile, err := os.Open(*path)
 
-	// avvio di 3 workers
-	for w := 1; w <= 3; w++ {
-		go worker(w, jobs, results)
+	if err != nil {
+		log.Fatal("File non trovato")
+	}
+	defer csvFile.Close()
+
+	reader := csv.NewReader(csvFile)
+	content, _ := reader.ReadAll()
+
+	if len(content) < 1 {
+		log.Fatal("Qualcosa non va, il file potrebbe essere vuoto o inconsistente")
 	}
 
-	// invio i jobs ai worker avviati in precedenza
-	// poi chiudo il channel per indicare di avere finito
-	for j := 1; j <= numJobs; j++ {
-		jobs <- j
+	headersArr := make([]string, 0)
+	for _, headE := range content[0] {
+		headersArr = append(headersArr, headE)
 	}
-	close(jobs)
 
-	// risultato
-	for a := 1; a <= numJobs; a++ {
-		<-results
+	//Remove the header row
+	content = content[1:]
+
+	var buffer bytes.Buffer
+	buffer.WriteString("[")
+	for i, d := range content {
+		buffer.WriteString("{")
+		for j, y := range d {
+			buffer.WriteString(`"` + headersArr[j] + `":`)
+			_, fErr := strconv.ParseFloat(y, 32)
+			_, bErr := strconv.ParseBool(y)
+			if fErr == nil {
+				buffer.WriteString(y)
+			} else if bErr == nil {
+				buffer.WriteString(strings.ToLower(y))
+			} else {
+				buffer.WriteString((`"` + y + `"`))
+			}
+			//end of property
+			if j < len(d)-1 {
+				buffer.WriteString(",")
+			}
+
+		}
+		//end of object of the array
+		buffer.WriteString("}")
+		if i < len(content)-1 {
+			buffer.WriteString(",")
+		}
+	}
+
+	buffer.WriteString(`]`)
+	rawMessage := json.RawMessage(buffer.String())
+	x, _ := json.MarshalIndent(rawMessage, "", "  ")
+	newFileName := filepath.Base(*path)
+	newFileName = newFileName[0:len(newFileName)-len(filepath.Ext(newFileName))] + ".json"
+	r := filepath.Dir(*path)
+	return x, filepath.Join(r, newFileName)
+}
+
+// SaveFile Will Save the file, magic right?
+func SaveFile(myFile []byte, path string) {
+	if err := ioutil.WriteFile(path, myFile, os.FileMode(0644)); err != nil {
+		panic(err)
 	}
 }
